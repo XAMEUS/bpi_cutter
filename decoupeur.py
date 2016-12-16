45d1573742775e9b74bc13e2443adcad2f1d79d5
Julie Saouli <Julie.Saouli@grenoble-inp.org>
2016-12-16 20:31:11 +0100

    correction cas particuliers + cleaned

--- decoupeur.py
+++ decoupeur.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-Module principal pour la découpe de modele 3D
+Module principal pour la découpe de modèles 3D
 """
 
 import argparse
@@ -9,9 +9,24 @@ import itertools
 from stl import STL
 from svg import SVG
 
+
+def triangle_in_slice(triangle, z_slice):
+    """
+    Teste si le triangle est dans la tranche
+    """
+    return sum(1 for point in triangle if point[2] == z_slice) // 3
+
+
+def segment_in_slice(point0, point1, z_slice):
+    """
+    Teste si le segment est dans la tranche
+    """
+    return point0[2] == z_slice and point1[2] == z_slice
+
+
 def segment_intersects_slice(point0, point1, z_slice):
     """
-    Calcul le parametre qui teste si les deux points forment un segment qui intersecte la tranche
+    Calcule le parametre qui teste si les deux points forment un segment qui intersecte la tranche
     """
     denominateur = point1[2] - point0[2]
     numerateur = z_slice - point0[2]
@@ -19,10 +34,6 @@ def segment_intersects_slice(point0, point1, z_slice):
         parameter = numerateur/denominateur
         if parameter >= 0 and parameter <= 1:
             return parameter
-    else:
-        if numerateur == 0: #segment dans le plan
-            return 'in'
-
 
 
 def calcul_intersection(point0, point1, parameter):
@@ -31,34 +42,34 @@ def calcul_intersection(point0, point1, parameter):
     """
     return [point0[i] + parameter*(point1[i] - point0[i]) for i in range(3)]
 
+
 def intersection(triangle, z_slice):
     """
-    Iterateur sur les points d'intersection du triangle avec la tranche
+    Calcul des points d'intersection du triangle avec la tranche
     """
     points = []
-    for couple in itertools.combinations(triangle.vertices, 2):
-        parameter = segment_intersects_slice(couple[0], couple[1], z_slice)
-        if parameter == 'in':
-            points.append(couple[0])
-            points.append(couple[1]) #marche si triangle dans plan ?
-        elif parameter is not None:
-            points.append(calcul_intersection(couple[0], couple[1], parameter))
+    for point0, point1 in itertools.combinations(triangle.vertices, 2):
+        if segment_in_slice(point0, point1, z_slice) and not triangle_in_slice(triangle, z_slice):
+            points.append(point0)
+            points.append(point1)
+        else:
+            parameter = segment_intersects_slice(point0, point1, z_slice)
+            if parameter:
+                points.append(calcul_intersection(point0, point1, parameter))
     return points
 
-def generate_slice(stl, z_slice, svg):
+
+def generate_slice(stl, z_slice, svg, origins, scale):
     """
-    generation d'un tranche
+    Generation d'un tranche
     """
     for triangle in stl.triangles:
         points = intersection(triangle, z_slice)
-        if points != []:
-            # print(points) #temp
-        # if len(points) == 1:
-            # svg.point(filename, point[0])
-        # elif len(points) >= 2:
-            for couple in itertools.combinations(points, 2):
-                # svg.segment(filename, points[0], points[1])
-                svg.add_line(points[0], points[1])
+        for point0, point1 in itertools.combinations(points, 2):
+            for index in range(2):
+                point0[index] = (point0[index] - origins[index])*scale
+                point1[index] = (point1[index] - origins[index])*scale
+            svg.add_line(point0, point1)
 
 
 def main():
@@ -68,22 +79,23 @@ def main():
     parser = argparse.ArgumentParser(description="slice a binary STL file")
     parser.add_argument("stl_file", help="name of the binary STL file to slice")
     parser.add_argument("-s", "--slices", type=int, dest="SLICES", default=4,
-                        help="how many slices you want (default = 4)")
+                        help="how many slices you want (default is 4)")
+    parser.add_argument("-g", "--grow", type=int, dest="SCALE", default=100,
+                        help="scale up or down the slices (default is scale up by 100)")
     args = parser.parse_args()
 
     stl = STL()
     stl.load(args.stl_file)
-    dimension, z_start = stl.dimensions()
+    dimension, origins = stl.dimensions()
     height = dimension[2]/args.SLICES
 
     for index in range(args.SLICES):
-        z_slice = z_start + height*index
+        z_slice = origins[2] + height*index
         filename = "slice{}.svg".format(index)
-        # svg.head(filename, dimension[0], dimension[1])
-        svg = SVG(dimension[0], dimension[1])
-        generate_slice(stl, z_slice, svg)
-        # svg.tail(filename)
+        svg = SVG(dimension[0]*args.SCALE, dimension[1]*args.SCALE)
+        generate_slice(stl, z_slice, svg, origins, args.SCALE)
         svg.save(filename)
+
 
 if __name__ == "__main__":
     main()
