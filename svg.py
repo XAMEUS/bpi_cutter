2fec25745bb73c45648aeb67b4421a7e4a285fb8
Julie Saouli <Julie.Saouli@grenoble-inp.org>
2016-12-16 20:35:25 +0100

    correction code svg  + comm fonctions

--- svg.py
+++ svg.py
@@ -6,12 +6,15 @@ class SVG:
     """
     SVG object
     """
-    def __init__(self, width ,height):
-        self.head = """<svg width="{}" height="{}">""".format(width, height)
-        self.tail = "</svg>"
+    def __init__(self, width, height):
+        self.head = """<svg width="{}" height="{}">\n""".format(width, height)
+        self.tail = "</svg>\n"
         self.svgs = []
 
     def save(self, filename):
+        """
+        Cree un fichier svg
+        """
         with open(filename, "w") as fsave:
             fsave.write(self.head)
             for svg in self.svgs:
@@ -19,5 +22,10 @@ class SVG:
             fsave.write(self.tail)
 
     def add_line(self, point1, point2, couleur="rgb(255,0,0)", stroke_width=2):
-        self.svgs.append("""<line x1="{}" y1="{}" x2="{}" y2="{}" style="color:{};stroke-width:{}" />""".
-                            format(point1[0], point1[1], point2[0], point2[1], couleur, stroke_width))
+        """
+        Trace une ligne entre les points
+        """
+        self.svgs.append(
+            """<line x1="{}" y1="{}" x2="{}" y2="{}" style="stroke:{};stroke-width:{}"/>\n""".
+            format(point1[0], point1[1], point2[0], point2[1],
+                   couleur, stroke_width))
